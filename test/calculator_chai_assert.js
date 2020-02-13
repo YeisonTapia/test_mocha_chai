98156bb5391a7f13d5d1bee6cbcc0c9c9a30790e
YeisonTapia <yftapial@ut.edu.co>
2020-02-13 16:28:45 -0500

    Add asserts for function 'sumElements' for testing.

--- test/calculator_chai_assert.js
+++ test/calculator_chai_assert.js
@@ -1,6 +1,8 @@
 var assert    = require("chai").assert;
 var calculator = require("../app/calculator");
 describe("Calcultator tests using ASSERT interface from CHAI module: ", function() {
+
+  /* addTested function */
   describe("Check addTested Function: ", function() {
     it("Check the returned value using: assert.equal(value,'value'): ", function() {
       result   = calculator.addTested("text");
@@ -15,5 +17,20 @@ describe("Calcultator tests using ASSERT interface from CHAI module: ", function
       assert.lengthOf(result, 11);
     });
   });
+
+  /* sumElements function */
+  describe("Check sumElements Function: ", function() {
+    it("Check the returned value using: assert.equal(value,'value'): ", function() {
+      let arr = [1,2,3,4,5,6,7,8,9,10]
+      result = calculator.sumElements(arr);
+      assert.equal(result, 55);
+    });
+    it("Check the returned value using: assert.typeOf(value,'value'): ", function() {
+      let arr = [1,2,3,4,5,6,7,8,9,10]
+      result   = calculator.sumElements(arr);
+      assert.typeOf(result, "number");
+    });
+  });
+
 });
 
