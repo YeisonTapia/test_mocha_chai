6b82cc20eca99bcd6b84fd2e5cd3738d5bc11422
YeisonTapia <yftapial@ut.edu.co>
2020-02-13 16:31:12 -0500

    Add comments in each function with info about of how work.

--- app/calculator.js
+++ app/calculator.js
@@ -1,8 +1,10 @@
+/* this function only add  'tested' to end of string */
 exports.addTested = function(value) {
   var result = value + " tested";
   return result;
 };
 
+/* This function add all element of a array */
 exports.sumElements = function (arr) {
   return arr.length * (arr.length + 1)/2;
 }
