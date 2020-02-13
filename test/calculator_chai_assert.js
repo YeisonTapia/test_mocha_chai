ada82678d0687713dc1aa6b87b03c594065dd9e5
YeisonTapia <yftapial@ut.edu.co>
2020-02-13 16:35:27 -0500

    Update, use only 1 var in second function for testing.

--- test/calculator_chai_assert.js
+++ test/calculator_chai_assert.js
@@ -20,13 +20,12 @@ describe("Calcultator tests using ASSERT interface from CHAI module: ", function
 
   /* sumElements function */
   describe("Check sumElements Function: ", function() {
+    let arr = [1,2,3,4,5,6,7,8,9,10]
     it("Check the returned value using: assert.equal(value,'value'): ", function() {
-      let arr = [1,2,3,4,5,6,7,8,9,10]
       result = calculator.sumElements(arr);
       assert.equal(result, 55);
     });
     it("Check the returned value using: assert.typeOf(value,'value'): ", function() {
-      let arr = [1,2,3,4,5,6,7,8,9,10]
       result   = calculator.sumElements(arr);
       assert.typeOf(result, "number");
     });
